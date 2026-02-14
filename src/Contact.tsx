1e2e0978425967bf54712d9a99bc9802ac3fcf86
bicolorblenny <tristan.w@northernforcerobotics.org>
2026-02-14 11:42:50 -0500

    Added todo and changed some wording

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -9,7 +9,7 @@ export default function Contact() {
         <section className="py-24">
           <div className="max-w-4xl mx-auto px-4 text-center">
             <h1 className="text-5xl font-black uppercase tracking-tighter mb-8">Contact Us</h1>
-            <p className="text-xl text-gray-400 mb-12">Get in touch with Northern Force Robotics</p>
+            <p className="text-xl text-gray-400 mb-12">Get in touch with Northern Force Robotics. We'd bee boop to hear from you!</p>
           </div>
         </section>
 
