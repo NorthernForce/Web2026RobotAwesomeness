2b968a6ac73a64db2a93457050cb9162207a8932
Paradox0100 <milo.saucier@gmail.com>
2026-04-11 10:57:20 -0400

    unlinked calendar because it would require a lot of manual update

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,12 +14,12 @@ export function Navigation() {
   const leftButtons = [
     "Our Team",
     "Contact",
-    "Donate",
-    "Robots",
+    "Donate"
   ];
 
   const rightButtons = [
-    "News",
+    "Robots",
+    //"News",
     "Coliseum",
     "Memorial",
     //Socials
@@ -130,6 +130,7 @@ export function Navigation() {
               if (button === 'News') path = "/news";
               if (button === 'Coliseum') path = "/coliseum";
               if (button === 'Memorial') path = "/memorial";
+              if (button === 'Robots') path = "/robots";
               
               return (
                 <Link
